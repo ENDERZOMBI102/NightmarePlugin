affe21e7646c9a1faa8edb8cc636a5a5d83ee4a9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-05-10 23:06:10 +0200

    commented and documented

--- src/Modules/Module.hpp
+++ src/Modules/Module.hpp
@@ -7,11 +7,18 @@ public:
 
 public:
     virtual ~Module() = default;
+    /**
+     * Starts this module
+     * @return whether this module initialization was successful
+     */
     virtual bool Init() = 0;
+    /**
+     * stops this module
+     */
     virtual void Shutdown() = 0;
     /**
      * Get the module name
-     * */
+     */
     virtual const char* Name() = 0;
 };
 
@@ -21,19 +28,38 @@ public:
 
 public:
     Modules();
+    /**
+     * Registers a module
+     * @tparam T module type
+     * @param modulePtr pointer to a module
+     */
     template <typename T = Module>
     void AddModule(T** modulePtr)
     {
         *modulePtr = new T();
         this->list.push_back(*modulePtr);
     }
+    /**
+     * Unregisters a module
+     * @tparam T module type
+     * @param modulePtr pointer to a module
+     */
     template <typename T = Module>
     void RemoveModule(T** modulePtr)
     {
         this->list.erase(*modulePtr);
     }
+    /**
+     * Initialize all registered modules
+     */
     void InitAll();
+    /**
+     * Stops all registered modules
+     */
     void ShutdownAll();
+    /**
+     * Unregister all registered modules
+     */
     void DeleteAll();
     ~Modules();
 };
