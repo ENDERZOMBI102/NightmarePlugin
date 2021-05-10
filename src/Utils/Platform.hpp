0e521c50a3edef8fde0b6ca3f8e94a625410016e
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-05-10 23:41:36 +0200

    badly documented

--- src/Utils/Platform.hpp
+++ src/Utils/Platform.hpp
@@ -8,26 +8,47 @@
 #define GAME_PATH(x) _GAME_PATH(Games/Windows/##x.hpp)
 // clang-format on
 #define __func __thiscall
+/**
+ * mark something to be exported from the plugin's dll
+ */
 #define DLL_EXPORT extern "C" __declspec(dllexport)
 
+/**
+ * https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR(name, ...)                                  \
     using _##name = int(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                        \
     static int __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__);
+/**
+ * https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR_T(type, name, ...)                           \
     using _##name = type(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                         \
     static type __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__);
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR_B(name, ...)                                \
     using _##name = int(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                        \
     static _##name name##Base;                                  \
     static int __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__);
 
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR(name, ...) \
     int __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__)
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR_T(type, name, ...) \
     type __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__)
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR_B(name, ...) \
     int __fastcall name##_Hook(void* thisptr, int edx, ##__VA_ARGS__)
 #else
@@ -39,33 +60,61 @@
 #define __cdecl __attribute__((__cdecl__))
 #define __stdcall __attribute__((__stdcall__))
 #define __fastcall __attribute__((__fastcall__))
+/**
+ * mark something to be exported from the plugin's dll
+ */
 #define DLL_EXPORT extern "C" __attribute__((visibility("default")))
 
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR(name, ...)                                  \
     using _##name = int(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                        \
     static int __func name##_Hook(void* thisptr, ##__VA_ARGS__);
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR_T(type, name, ...)                           \
     using _##name = type(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                         \
     static type __func name##_Hook(void* thisptr, ##__VA_ARGS__);
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR_B(name, ...)                                \
     using _##name = int(__func*)(void* thisptr, ##__VA_ARGS__); \
     static _##name name;                                        \
     static _##name name##Base;                                  \
     static int __func name##_Hook(void* thisptr, ##__VA_ARGS__);
 
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR(name, ...) \
     int __func name##_Hook(void* thisptr, ##__VA_ARGS__)
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR_T(type, name, ...) \
     type __func name##_Hook(void* thisptr, ##__VA_ARGS__)
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR_B(name, ...) \
     int __func name##_Hook(void* thisptr, ##__VA_ARGS__)
 #endif
 
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DECL_DETOUR_STD(type, name, ...)             \
     using _##name = type(__stdcall*)(##__VA_ARGS__); \
     static _##name name;                             \
     static type __stdcall name##_Hook(##__VA_ARGS__);
+
+/**
+ *  https://github.com/Krzyhau/SourceAutoRecord/blob/master/doc/contributing.md#hooking
+ */
 #define DETOUR_STD(type, name, ...) \
     type __stdcall name##_Hook(##__VA_ARGS__)
